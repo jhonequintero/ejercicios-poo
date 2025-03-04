b7385f1a9f4d56723df82076919394a21c4a4f6d
jhonequintero <jhoneiderrodriguez6@gmail.com>
2025-03-04 08:00:17 -0500

    ..
    8:00 am

--- README.md
+++
@@ -1,1 +0,0 @@
-# ejercicios-poo
---
+++ SistemaLuces.java
@@ -0,0 +1,17 @@
+
+
+
+public class SistemaLuces {
+
+    public void conMovimiento() {
+        System.out.println("se nos metieron al rancho menoress saquen los machetes ");
+        System.out.println("se  encendieronn las lucess ");
+
+    }
+
+    public void sinmovimiento() {
+        System.out.println("relajesen y duerman tranquilo");
+        System.out.println("se apagaron las luces");
+    }
+
+}
